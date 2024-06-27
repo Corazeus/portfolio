8c7188491428d6d10a7ded7a83a5f028170d720f
Corazeus <ruizrenzeus@gmail.com>
2024-06-28 02:03:56 +0800

    Updated Hero Information

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,9 +6,8 @@ const Hero = () => {
     <div className='justify-center items-center text-center flex flex-col mt-12'>
       <Message/>
         <img src="./images/image0-square.jpeg" alt="Profile" className="w-100 h-80 display-picture mb-5 glow-image"/>
-        <p className='text-xl font-extrabold'>Renzeus Marrius B. Ruiz</p>
-        <p className='text-blue-500 font-light'>Accenture Inc.,  Associate Software Engineer</p>
-        <p className='text-xs font-thin'>Current Position</p>
+        <p className='text-blue-500 font-light text-base'>Accenture Inc.,  Associate Software Engineer</p>
+        <p className='text-sm font-extralight'>Current Position</p>
         <a href="./images/cv.pdf" download className='mt-10'>
           <button className='btn-primary p-2 bg-sky-500 rounded bg-opacity-50'>DOWNLOAD CV</button>
         </a>
