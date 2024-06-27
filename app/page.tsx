19009ffa516a88fd43f150ad0353624058b55bd7
Corazeus <ruizrenzeus@gmail.com>
2024-06-28 01:32:29 +0800

    Improved for Mobile View

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const skills = ["SKILLS"];
 
   return (
-    <main className="flex flex-col items-center justify-center mx-auto sm:px-10 px-5 overflow-hidden overflow-y-hidden w-[calc(100%-4rem)] rounded-md min-h-screen">
+    <main className="flex flex-col items-center justify-center mx-auto sm:px-16 px-10 overflow-hidden w-screen rounded-md min-h-screen">
       <section id="profile" className="min-h-screen pt-10">
         <Vortex rangeY={3000} className="flex items-center flex-col justify-center">
           <Navigation/>
@@ -24,12 +24,12 @@ export default function Home() {
       </section>
       <section id="experience" className="min-h-screen pt-10">
         <Vortex rangeY={3000} className="flex items-center flex-col justify-center">
-          <p>EXPERIENCE</p>
+          <p className="text-5xl">EXPERIENCE</p>
         </Vortex>
       </section>
       <section id="about" className="min-h-screen pt-10">
         <Vortex rangeY={7000} className="flex items-center flex-col justify-center">
-          <p>ABOUT</p>
+          <p className="text-5xl">ABOUT</p>
         </Vortex>
       </section>
     </main>
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -7,44 +7,51 @@ const TechStack = () => {
 
     const people = [
         {
-          id: 1,
-          name: "Java",
-          designation: "Springboot, Maven, Gradle",
-          image:
-            "/images/java-logo.svg",
+            id: 1,
+            name: "Git",
+            designation: "Collboration, Repositories, Version Control, etc...",
+            image:
+                "/images/git.png",
         },
         {
-          id: 2,
-          name: "Javascript",
-          designation: "React, Quasar, Vue, Node, Next",
-          image:
-            "/images/javascript.png",
+            id: 2,
+            name: "Amazon Web Sevices",
+            designation: "S3, API Gateway, Lambda, CLI, EC2, DynamoDB",
+            image:
+              "/images/amazon.png",
         },
         {
-          id: 3,
-          name: "Python",
-          designation: "Django, FastAPI",
-          image:
-            "/images/python.jfif",
+            id: 3,
+            name: "Java",
+            designation: "Springboot, Maven, Gradle",
+            image:
+                "/images/java-logo.svg",
         },
         {
-          id: 4,
-          name: "Amazon Web Sevices",
-          designation: "S3, API Gateway, Lambda, CLI, EC2, DynamoDB",
-          image:
-            "/images/amazon.png",
+            id: 4,
+            name: "Javascript",
+            designation: "React, Quasar, Vue, Node, Next",
+            image:
+                "/images/javascript.png",
         },
         {
-          id: 5,
-          name: "Git",
-          designation: "",
-          image:
-            "/images/git.png",
+            id: 5,
+            name: "Python",
+            designation: "Django, FastAPI",
+            image:
+                "/images/python.jfif",
+        },
+        {
+            id: 6,
+            name: "Others",
+            designation: "C, C#, PHP, MySQL, MariaDB",
+            image:
+                "/images/ellipsis.jpg",
         },
       ];
 
   return (
-    <div className="flex flex-row items-center justify-center mb-10 w-full pt-80">
+    <div className="flex flex-row items-center justify-center mb-10 w-full pt-64 px-5">
       <AnimatedTooltip items={people}/>
     </div>
   )
