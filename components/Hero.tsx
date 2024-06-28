d1d7435982dde63d27c155d4df454a20b3b7b92f
Corazeus <ruizrenzeus@gmail.com>
2024-06-28 18:10:08 +0800

    Added rarget="_blank" to Download CV button to open in a new tab

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <img src="./images/image0-square.jpeg" alt="Profile" className="w-100 h-80 display-picture mb-5 glow-image"/>
         <p className='text-blue-500 font-light text-base'>Accenture Inc.,  Associate Software Engineer</p>
         <p className='text-sm font-extralight'>Current Position</p>
-        <a href="./images/cv.pdf" download className='mt-10'>
+        <a href="./images/cv.pdf" download className='mt-10' target='_blank'>
           <button className='btn-primary p-2 bg-sky-500 rounded bg-opacity-50'>DOWNLOAD CV</button>
         </a>
     </div>
