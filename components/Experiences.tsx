555b916f4b30ea870cb708d3d592d19328b23014
Corazeus <ruizrenzeus@gmail.com>
2024-06-28 16:18:09 +0800

    Changed animation direction of cards from bottom x-axis to y-axis

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -44,17 +44,17 @@ const Experiences = () => {
     const variants = {
         enter: (direction: number) => {
             return {
-                x: direction > 0 ? 1000 : -1000,
+                y: direction > 0 ? 1000 : -1000,
                 opacity: 0
             };
         },
         center: {
-            x: 0,
+            y: 0,
             opacity: 1
         },
         exit: (direction: number) => {
             return {
-                x: direction < 0 ? 1000 : -1000,
+                y: direction < 0 ? 1000 : -1000,
                 opacity: 0
             };
         }
