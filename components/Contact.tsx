6043b4b0bb55be3f81cf163f3037eef6b6615f98
Corazeus <ruizrenzeus@gmail.com>
2024-07-01 20:32:11 +0800

    Updated Contact Icons

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { EnvelopeIcon, NewspaperIcon } from '@heroicons/react/16/solid'
+import { EnvelopeIcon, ListBulletIcon } from '@heroicons/react/16/solid'
 import React from 'react'
 
 const Contact = () => {
@@ -9,8 +9,8 @@ const Contact = () => {
             <h1 className='mb-3 font-medium text-center'> Contact Me </h1>
             <div className='flex flex-row space-x-2 justify-center'>
                 <a href='https://linkedin.com/in/ruizrenzeus' target='_blank'><img src='/images/in.png' className="h-11 w-11 hover:opacity-50 rounded-lg"/></a>
-                <a href='https://forms.gle/BJrE6BozVRLhZqMB9' target='_blank'><NewspaperIcon className="h-15 w-12 hover:text-gray-700"/></a>
                 <a href='mailto:ruizrenzeus@gmail.com' target='_blank'><EnvelopeIcon className="h-15 w-12 hover:text-gray-700"/></a>
+                <a href='https://forms.gle/BJrE6BozVRLhZqMB9' target='_blank'><ListBulletIcon className="h-15 w-12 hover:text-gray-700"/></a>
             </div>
         </div>
     </div>
